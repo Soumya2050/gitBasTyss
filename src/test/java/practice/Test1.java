577060aedcf62c9d1ee295a7d658eec8345c09e5
Soumya2050 <srjena2050@gmail.com>
2023-07-12 11:00:16 +0530

    2nd commit

--- src/test/java/practice/Test1.java
+++ src/test/java/practice/Test1.java
@@ -1,10 +1,15 @@
 package practice;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Test1 {
-	
-	public static void main(String[] args) {
+	@Test
+	public void test() {
 		
 		System.out.println("hiii");
+		System.out.println("hello");
+		Reporter.log("Hii Good Morning");
 	}
 	
 }
