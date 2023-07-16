cd8d4e136ce27a5206db43aa646d6287523370f8
Soumya2050 <srjena2050@gmail.com>
2023-07-16 12:54:08 +0530

    My fourth commit

---
+++ src/test/java/apractice/BrokenLinks.java
@@ -0,0 +1,50 @@
+package apractice;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BrokenLinks {
+	
+	@Test
+	public void brokenLinks() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://rmgtestingserver/domain/House_Rental_Application/");
+		List<WebElement> links = driver.findElements(By.xpath("//a"));
+		ArrayList<Object> brokenList = new ArrayList<Object>();
+		
+		for(int i=0;i<links.size();i++)
+		{
+			String allLinks = links.get(i).getAttribute("href");
+			
+			URL url;
+			int statusCode = 0;
+			try {
+				url = new URL(allLinks);
+				URLConnection connection = url.openConnection();
+				HttpURLConnection httpconnection = (HttpURLConnection)connection;
+				statusCode = httpconnection.getResponseCode();
+				if(statusCode>=400) {
+					brokenList.add(allLinks+"------>"+ statusCode);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(brokenList);
+			driver.quit();
+			
+		}
+	}
+
+}
---
+++ src/test/java/apractice/MakeMyTripTask1.java
@@ -0,0 +1,48 @@
+package apractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class MakeMyTripTask1 {
+	
+	@Test
+	public void test1() throws InterruptedException {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("https://www.makemytrip.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		Actions a = new Actions(driver);
+		a.click().perform();
+		driver.findElement(By.xpath("//label[@for='fromCity']")).click();
+		driver.findElement(By.xpath("//input[@placeholder='From']")).sendKeys("Bengaluru");
+		
+		WebElement from = driver.findElement(By.xpath("//div[@class='calc60']/p[contains(text(),'Bengaluru')]"));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(from)).click();
+		
+		driver.findElement(By.xpath("//label[@for='toCity']")).click();
+		driver.findElement(By.xpath("//input[@placeholder='To']")).sendKeys("Bhubaneswar");
+		
+		WebElement to = driver.findElement(By.xpath("//div[@class='calc60']/p[contains(text(),'Bhubaneswar')]"));
+		wait.until(ExpectedConditions.elementToBeClickable(to)).click();
+		
+		driver.findElement(By.xpath("//div[contains(text(),'July')]/../following-sibling::div/div/descendant::p[text()=20]")).click();
+		driver.findElement(By.xpath("//a[text()='Search']")).click();
+		
+		a.click();
+		
+		
+	}
+	
+}
